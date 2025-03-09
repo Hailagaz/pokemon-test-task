02c34b68f790370b700dd6914fed0e2e7d81a02d
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2025-03-09 15:52:34 +0200

    changed fields

--- pokemon-form/src/components/PokemonForm.tsx
+++ pokemon-form/src/components/PokemonForm.tsx
@@ -18,39 +18,30 @@ export default function PokemonForm() {
 				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 					<form action="#" method="POST" className="space-y-6">
 						<div>
-							<label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-								Email address
+							<label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
+								First name
 							</label>
 							<div className="mt-2">
 								<input
-									id="email"
-									name="email"
-									type="email"
-									required
-									autoComplete="email"
+									id="first-name"
+									name="first-name"
+									type="text"
+									autoComplete="given-name"
 									className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
 								/>
 							</div>
 						</div>
 
-						<div>
-							<div className="flex items-center justify-between">
-								<label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-									Password
-								</label>
-								<div className="text-sm">
-									<a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-										Forgot password?
-									</a>
-								</div>
-							</div>
+						<div className="sm:col-span-3">
+							<label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
+								Last name
+							</label>
 							<div className="mt-2">
 								<input
-									id="password"
-									name="password"
-									type="password"
-									required
-									autoComplete="current-password"
+									id="last-name"
+									name="last-name"
+									type="text"
+									autoComplete="family-name"
 									className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
 								/>
 							</div>
@@ -63,7 +54,7 @@ export default function PokemonForm() {
 								type="submit"
 								className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 							>
-								Sign in
+								Submit
 							</button>
 						</div>
 					</form>
